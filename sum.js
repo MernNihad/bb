cd682bf681f9cb7b87d0cf0658ea09880ca5c279
NIHAD O T <mernnihadsoft@gmail.com>
2023-09-08 14:52:04 +0530

    initail commit

---
+++ sum.js
@@ -0,0 +1,24 @@
+export function count(params) {
+    console.log(100);
+}export function count1(params) {
+    console.log(100);
+}
+
+export const name = 'name';
+
+export const arr = [
+    {
+        name:'name',
+        age:10,
+    },
+    {
+        name:'name',
+        age:10,
+    },{
+        name:'name',
+        age:10,
+    },{
+        name:'name',
+        age:10,
+    }
+]
